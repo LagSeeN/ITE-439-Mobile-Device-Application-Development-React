d51ebc9d2006e064bcdb4944ad87c8a952df9a72
LagSeeN <19259262+LagSeeN@users.noreply.github.com>
2020-12-01 13:59:23 +0700

    commit on master

--- App.js
+++ App.js
@@ -68,21 +68,92 @@
 
 //////////////////////////////////////////////
 
-import React, {Component} from 'react';
-import {Text, StyleSheet, View, TextInput, Alert, Button} from 'react-native';
+// import React, {Component} from 'react';
+// import {Text, StyleSheet, View, TextInput, Alert, Button} from 'react-native';
 
-export default class App extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      TextInputName: '',
-      TextInputEmail: '',
-    };
-  }
+// export default class App extends Component {
+//   constructor(props) {
+//     super();
+//     this.state = {
+//       TextInputName: '',
+//       TextInputEmail: '',
+//     };
+//   }
 
-  CheckTextInput = () => {
-    if (this.state.TextInputName != '') {
-      if (this.state.TextInputEmail != '') {
+//   CheckTextInput = () => {
+//     if (this.state.TextInputName != '') {
+//       if (this.state.TextInputEmail != '') {
+//         alert('Success');
+//       } else {
+//         alert('Please Input Email');
+//       }
+//     } else {
+//       alert('Please Input Name');
+//     }
+//   };
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <TextInput
+//           placeholder="Enter Name"
+//           underlineColorAndroid="transparent"
+//           style={styles.TextInput}
+//           onChangeText={(TextInputName) => this.setState({TextInputName})}
+//         />
+//         <TextInput
+//           placeholder="Enter Email"
+//           underlineColorAndroid="transparent"
+//           style={styles.TextInput}
+//           onChangeText={(TextInputEmail) => this.setState({TextInputEmail})}
+//         />
+//         <View style={{marginTop: 15}}>
+//           <Button
+//             title="Submit"
+//             color="#606070"
+//             onPress={this.CheckTextInput}
+//           />
+//         </View>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     // alignItems: 'center',
+//     // justifyContent: 'center',
+//     margin: 35,
+//   },
+//   TextInput: {
+//     paddingLeft: 5,
+//     margin: 5,
+//     height: 40,
+//     width: '100%',
+//     borderWidth: 1,
+//     borderColor: '#606070',
+//   },
+// });
+
+////////////////////////////
+
+import React, {Component, useState} from 'react';
+import {
+  Text,
+  StyleSheet,
+  View,
+  Button,
+  SafeAreaView,
+  TextInput,
+} from 'react-native';
+
+const App = () => {
+  const [TextInputName, setTextInputName] = useState('');
+  const [TextInputEmail, setTextInputEmail] = useState('');
+
+  const CheckTextInput = () => {
+    if (TextInputName != '') {
+      if (TextInputEmail != '') {
         alert('Success');
       } else {
         alert('Please Input Email');
@@ -91,19 +162,29 @@ export default class App extends Component {
       alert('Please Input Name');
     }
   };
-  render() {
-    return (
-      <View style={styles.container}>
-        <TextInput
-          placeholder="Enter Name"
-          underlineColorAndroid="transparent"
-          style={styles.TextInput}
-          onChangeText={(TextInputName) => this.setState({TextInputName})}
-        />
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        placeholder="Enter Name"
+        underlineColorAndroid="transparent"
+        style={styles.TextInput}
+        onChangeText={(TextInputName) => setTextInputName(TextInputName)}
+      />
+      <TextInput
+        placeholder="Enter Email"
+        underlineColorAndroid="transparent"
+        style={styles.TextInput}
+        onChangeText={(TextInputEmail) => setTextInputEmail(TextInputEmail)}
+      />
+      <View style={{marginTop: 15}}>
+        <Button title="Submit" color="#AA6070" onPress={CheckTextInput} />
       </View>
-    );
-  }
-}
+    </View>
+  );
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
@@ -121,3 +202,4 @@ const styles = StyleSheet.create({
     borderColor: '#606070',
   },
 });
+
