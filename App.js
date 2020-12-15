a15c148009823e8b3316be17d04648d7c00952cf
LagSeeN <19259262+LagSeeN@users.noreply.github.com>
2020-12-15 12:42:32 +0700

    add 12/15/2020 code

--- App.js
+++ App.js
@@ -97,6 +97,70 @@
 
 ////////////////////////////////////
 
+// import React, {useState} from 'react';
+// import {
+//   Text,
+//   StyleSheet,
+//   View,
+//   SafeAreaView,
+//   Modal,
+//   Button,
+// } from 'react-native';
+
+// const App = () => {
+//   const [showModal, setShowModal] = useState();
+
+//   return (
+//     <SafeAreaView style={{flex: 1}}>
+//       <View style={styles.contaner}>
+//         <Modal animationType={'slide'} transparent={false} visible={showModal}>
+//           <View style={styles.modal}>
+//             <Text style={styles.text}>Modal is open!</Text>
+//             <Button
+//               title="Click To Close Modal"
+//               onPress={() => {
+//                 setShowModal(!showModal);
+//               }}
+//             />
+//           </View>
+//         </Modal>
+//         <Button
+//           title="Click To Show Modal"
+//           onPress={() => {
+//             setShowModal(!showModal);
+//           }}
+//         />
+//       </View>
+//     </SafeAreaView>
+//   );
+// };
+
+// export default App;
+
+// const styles = StyleSheet.create({
+//   contaner: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     backgroundColor: '#ecf0f1',
+//     marginTop: 30,
+//   },
+//   modal: {
+//     flex: 1,
+//     alignItems: 'center',
+//     backgroundColor: 'blue',
+//     padding: 100,
+//   },
+//   text: {
+//     color: 'white',
+//     marginTop: 10,
+//     fontSize: 20,
+//     padding: 10,
+//   },
+// });
+
+/////////////////////////////////////////
+
 import React, {useState} from 'react';
 import {
   Text,
@@ -105,6 +169,7 @@ import {
   SafeAreaView,
   Modal,
   Button,
+  TouchableNativeFeedback,
 } from 'react-native';
 
 const App = () => {
@@ -112,24 +177,35 @@ const App = () => {
 
   return (
     <SafeAreaView style={{flex: 1}}>
-      <View style={styles.contaner}>
-        <Modal animationType={'slide'} transparent={false} visible={showModal}>
-          <View style={styles.modal}>
-            <Text style={styles.text}>Modal is open!</Text>
-            <Button
-              title="Click To Close Modal"
-              onPress={() => {
-                setShowModal(!showModal);
-              }}
-            />
+      <View style={styles.centeredView}>
+        <Modal animationType={'slide'} transparent={true} visible={showModal}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>
+                สวัสดีครับ/ค่ะ ไฟในห้องปิดอยู่ครับ
+              </Text>
+              <TouchableNativeFeedback
+                onPress={() => {
+                  setShowModal(!showModal);
+                }}
+                background={TouchableNativeFeedback.SelectableBackground()}>
+                <View style={styles.closeButton}>
+                  <Text style={styles.textStyle}> กรุณากดปุ่มเปิดไฟ </Text>
+                </View>
+              </TouchableNativeFeedback>
+            </View>
           </View>
         </Modal>
-        <Button
-          title="Click To Show Modal"
+        <Text style={styles.modalText}>คุณลืมปิดไฟในห้อง</Text>
+        <TouchableNativeFeedback
           onPress={() => {
             setShowModal(!showModal);
           }}
-        />
+          background={TouchableNativeFeedback.SelectableBackground()}>
+          <View style={styles.openButton}>
+            <Text style={styles.textStyle}> กรุณากดปุ่มปิดไฟ </Text>
+          </View>
+        </TouchableNativeFeedback>
       </View>
     </SafeAreaView>
   );
@@ -138,23 +214,47 @@ const App = () => {
 export default App;
 
 const styles = StyleSheet.create({
-  contaner: {
+  centeredView: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    marginTop: 30,
-  },
-  modal: {
-    flex: 1,
     alignItems: 'center',
-    backgroundColor: 'blue',
-    padding: 100,
+    marginTop: 22,
   },
-  text: {
-    color: 'white',
-    marginTop: 10,
-    fontSize: 20,
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+
+  openButton: {
+    backgroundColor: '#F194FF',
+    borderRadius: 5,
     padding: 10,
+    elevation: 2,
+  },
+  closeButton: {
+    backgroundColor: 'red',
+    borderRadius: 5,
+    padding: 10,
+    elevation: 2,
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
   },
 });
