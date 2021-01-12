c2aee0d0768a67eab0d912b3fac4a180e23a9b0f
LagSeeN <19259262+LagSeeN@users.noreply.github.com>
2021-01-12 14:08:39 +0700

    add เนื้อหาการเรียนของวันที่ 1/12/2020

--- App.js
+++ App.js
@@ -1,186 +1,105 @@
-// import React, {useState} from 'react';
-
-// import {SafeAreaView, StyleSheet, View, ActivityIndicator} from 'react-native';
-
-// import {WebView} from 'react-native-webview';
-
-// const ActivityIndicatorElement = () => {
-//   return (
-//     <View style={styles.activityIndicatorStyle}>
-//       <ActivityIndicator color="#009688" size="large" />
-//     </View>
-//   );
-// };
-
-// const App = () => {
-//   const [visible, setVisible] = useState(false);
-
-//   return (
-//     <SafeAreaView style={{flex: 1}}>
-//       <View style={styles.container}>
-//         <WebView
-//           style={{flex: 1}}
-//           source={{uri: 'https://www.google.com'}}
-//           javaScriptEnabled={true}
-//           domStorageEnabled={true}
-//           onLoadStart={() => setVisible(true)}
-//           onLoad={() => setVisible(false)}
-//         />
-//         {visible ? <ActivityIndicatorElement /> : null}
-//       </View>
-//     </SafeAreaView>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#F5FCFF',
-//   },
-//   activityIndicatorStyle: {
-//     flex: 1,
-//     position: 'absolute',
-//     margin: 'auto',
-//     left: 0,
-//     right: 0,
-//     top: 0,
-//     bottom: 0,
-//     justifyContent: 'center',
-//   },
-// });
-
-// export default App;
-
-///////////////////////////////
-
-// import React from 'react';
+// import React, {Component} from 'react';
 // import {
 //   Text,
 //   StyleSheet,
 //   View,
-//   SafeAreaView,
-//   SectionList,
-//   ScrollView,
+//   TouchableOpacity,
+//   FlatList,
+//   ActivityIndicator,
 // } from 'react-native';
 
-// const DATA = [
-//   {
-//     title: 'Main dishes',
-//     data: ['Pizza', 'Burger', 'Risotto'],
-//   },
-//   {
-//     title: 'Sides',
-//     data: ['French Fries', 'Onion Rings', 'Fried Shrimps'],
-//   },
-//   {
-//     title: 'Drinks',
-//     data: ['Water', 'Coke', 'Beer'],
-//   },
-//   {
-//     title: 'Desserts',
-//     data: ['Cheese Cake', 'Ice Cream'],
-//   },
-// ];
-
-// const Item = ({title}) => (
-//   <View style={styles.item}>
-//     <Text style={styles.title}>{title}</Text>
-//   </View>
-// );
-
-// const App = () => {
-//   return (
-//     <SafeAreaView style={styles.container}>
-//       <ScrollView>
-//         <SectionList
-//           sections={DATA}
-//           keyExtractor={(item, index) => item + index}
-//           renderItem={({item}) => <Item title={item} />}
-//           renderSectionHeader={({section: {title}}) => (
-//             <Text style={styles.header}>{title}</Text>
-//           )}
-//         />
-//       </ScrollView>
-//     </SafeAreaView>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     marginHorizontal: 20,
-//   },
-//   item: {
-//     backgroundColor: '#F9C2FF',
-//     padding: 20,
-//     marginVertical: 10,
-//   },
-//   title: {
-//     fontSize: 24,
-//   },
-//   header: {
-//     fontSize: 32,
-//     backgroundColor: '#ffff',
-//   },
-// });
-
-// export default App;
-
-//////////////////////
-
-// import React, {Component} from 'react';
-// import {Text, View, SectionList, StyleSheet, Alert} from 'react-native';
-
 // export default class App extends Component {
-//   renderSeparator = () => {
-//     return <View style={{height: 1, width: '100%', backgroundColor: '#000'}} />;
+//   //กำหนดค่าเริ่มต้นใน State
+//   constructor() {
+//     super();
+//     this.state = {
+//       //Loading state used while loading data for the first time.
+//       loading: true,
+//       //Data Source for the Flatlist
+//       serverData: [],
+//       //Loading state used while loading more data.
+//       fetching_from_server: false,
+//     };
+//     //index of the offset to load from Web API
+//     this.offset = 1;
+//   }
+
+//   componentDidMount() {
+//     //Sending the current offset with get request
+//     fetch('https://aboutreact.herokuapp.com/getpost.php?offset=' + this.offset)
+//       .then((response) => response.json())
+//       //Successful response from the API Call
+//       .then((responseJson) => {
+//         //After the response increasing the offset for the next API call.
+//         this.offset = this.offset + 1;
+//         this.setState({
+//           serverData: [...this.state.serverData, ...responseJson.results],
+//           loading: false,
+//         });
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   }
+
+//   loadMoreData = () => {
+//     this.setState({fetching_from_server: true}, () => {
+//       fetch(
+//         'https://aboutreact.herokuapp.com/getpost.php?offset=' + this.offset,
+//       )
+//         .then((response) => response.json())
+//         .then((responseJson) => {
+//           this.offset = this.offset + 1;
+//           this.setState({
+//             serverData: [...this.state.serverData, ...responseJson.results],
+//             fetching_from_server: false,
+//           });
+//         })
+//         .catch((error) => {
+//           console.log(error);
+//         });
+//     });
 //   };
-//   getListViewItem = (item) => {
-//     Alert.alert("Alert แฮร่", item);
-//   };
+
+//   renderFooter() {
+//     return (
+//       <View style={styles.footer}>
+//         <TouchableOpacity
+//           activeOpacity={0.9}
+//           onPress={this.loadMoreData}
+//           style={styles.loadMoreBtn}>
+//           <Text style={styles.btnText}>Load More</Text>
+//           {this.state.fetching_from_server ? (
+//             <ActivityIndicator color="white" style={{marginLeft: 10}} />
+//           ) : null}
+//         </TouchableOpacity>
+//       </View>
+//     );
+//   }
+
 //   render() {
 //     return (
 //       <View style={styles.container}>
-//         <SectionList
-//           sections={[
-//             {
-//               title: 'A',
-//               data: ['ALTERED', 'ABBY', 'ACTION U.S.A.', 'AMUCK', 'ANGUISH'],
-//             },
-//             {
-//               title: 'B',
-//               data: [
-//                 'BEST MEN',
-//                 'BEYOND JUSTICE',
-//                 'BLACK GUNN',
-//                 'BLOOD RANCH',
-//                 'BEASTIES',
-//               ],
-//             },
-//             {
-//               title: 'C',
-//               data: [
-//                 'CARTEL',
-//                 'CASTLE OF EVIL',
-//                 'CHANCE',
-//                 'COP GAME',
-//                 'CROSS FIRE',
-//               ],
-//             },
-//           ]}
-//           renderItem={({item}) => (
-//             <Text
-//               style={styles.item}
-//               onPress={this.getListViewItem.bind(this, item)}>
-//               {item}
-//             </Text>
-//           )}
-//           renderSectionHeader={({section}) => (
-//             <Text style={styles.sectionHeader}>{section.title}</Text>
-//           )}
-//           keyExtractor={(item, index) => index}
-//           ItemSeparatorComponent={this.renderSeparator}
-//         />
+//         {this.setState.loading ? (
+//           <ActivityIndicator size="large" />
+//         ) : (
+//           <FlatList
+//             style={{width: '100%'}}
+//             keyExtractor={(item, index) => index}
+//             data={this.state.serverData}
+//             renderItem={({item, index}) => (
+//               <View style={styles.item}>
+//                 <Text style={styles.text}>
+//                   {item.id}
+//                   {'.'}
+//                   {item.title.toUpperCase()}
+//                 </Text>
+//               </View>
+//             )}
+//             ItemSeparatorComponent={() => <View style={styles.separator} />}
+//             ListFooterComponent={this.renderFooter.bind(this)}
+//           />
+//         )}
 //       </View>
 //     );
 //   }
@@ -189,108 +108,195 @@
 // const styles = StyleSheet.create({
 //   container: {
 //     flex: 1,
-//     backgroundColor: '#5ead97',
-//   },
-//   sectionHeader: {
-//     paddingTop: 2,
-//     paddingLeft: 10,
-//     paddingRight: 10,
-//     paddingBottom: 2,
-//     fontSize: 22,
-//     fontWeight: 'bold',
-//     color: '#FFF',
-//     backgroundColor: '#8fb1aa',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     paddingTop: 30,
 //   },
 //   item: {
 //     padding: 10,
-//     fontSize: 18,
-//     height: 44,
+//   },
+//   separator: {
+//     height: 0.5,
+//     backgroundColor: 'rgba(0,0,0,0.4)',
+//   },
+//   text: {
+//     fontSize: 15,
+//     color: 'black',
+//   },
+//   footer: {
+//     padding: 10,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     flexDirection: 'row',
+//   },
+//   loadMoreBtn: {
+//     padding: 10,
+//     backgroundColor: '#800000',
+//     borderRadius: 4,
+//     flexDirection: 'row',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   btnText: {
+//     color: 'white',
+//     fontSize: 15,
+//     textAlign: 'center',
 //   },
 // });
 
-/////////////////////////
+///////////////////////////////////
 
-import React, {Component} from 'react';
-import {Text, View, StyleSheet, FlatList, Alert} from 'react-native';
+// import React, {Component} from 'react';
+// import {
+//   Text,
+//   View,
+//   StyleSheet,
+//   TextInput,
+//   FlatList,
+//   ActivityIndicator,
+//   Alert,
+// } from 'react-native';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props),
-      (this.state = {
-        FlatListItems: [
-          {id: '1', value: 'A'},
-          {id: '2', value: 'B'},
-          {id: '3', value: 'C'},
-          {id: '4', value: 'D'},
-          {id: '5', value: 'E'},
-          {id: '6', value: 'F'},
-          {id: '7', value: 'G'},
-          {id: '8', value: 'H'},
-          {id: '9', value: 'I'},
-          {id: '10', value: 'J'},
-          {id: '11', value: 'K'},
-          {id: '12', value: 'L'},
-          {id: '13', value: 'M'},
-          {id: '14', value: 'N'},
-          {id: '15', value: 'O'},
-          {id: '16', value: 'P'},
-          {id: '17', value: 'Q'},
-          {id: '18', value: 'R'},
-          {id: '19', value: 'S'},
-          {id: '20', value: 'T'},
-          {id: '21', value: 'U'},
-          {id: '22', value: 'V'},
-          {id: '23', value: 'W'},
-          {id: '24', value: 'X'},
-          {id: '25', value: 'Y'},
-        ],
-      });
-  }
-  FlatListItemSeparator = () => {
-    return (
-      <View
-        style={{height: 0.5, width: '100%', backgroundColor: '#C8C8C8'}}></View>
-    );
-  };
-  GetItems(item) {
-    Alert.alert('Alert แฮร่', item);
-  }
-  render() {
-    return (
-      <View style={styles.container}>
-        <FlatList
-          data={this.state.FlatListItems}
-          ItemSeparatorComponent={this.FlatListItemSeparator}
-          renderItem={({item}) => (
-            <View>
-              <Text
-                style={styles.item}
-                onPress={this.GetItems.bind(
-                  this,
-                  'ID: ' + item.id + '\nValue: ' + item.value,
-                )}>
-                {item.value}
-              </Text>
-            </View>
-          )}
-        />
-      </View>
-    );
-  }
+// export class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isLoading: true,
+//       text: '',
+//     };
+//     this.arrayholder = [];
+//   }
+
+//   componentDidMount() {
+//     return fetch('https://jsonplaceholder.typicode.com/posts')
+//       .then((response) => response.json())
+//       .then((responseJson) => {
+//         this.setState(
+//           {
+//             isLoading: false,
+//             dataSource: responseJson,
+//           },
+//           () => {
+//             this.arrayholder = responseJson;
+//           },
+//         );
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   }
+
+//   searchFilterFunction(text) {
+//     const newData = this.arrayholder.filter((item) => {
+//       const itemData = item.title ? item.title.toUpperCase() : '';
+//       const textData = text.toUpperCase();
+//       return itemData.indexOf(textData) > -1;
+//     });
+//     this.setState({
+//       dataSource: newData,
+//       text: text,
+//     });
+//   }
+
+//   listViewItemSeparator = () => {
+//     return <View style={styles.separator}></View>;
+//   };
+
+//   render() {
+//     if (this.state.isLoading)
+//       return (
+//         <View style={{flex: 1, paddingTop: 20}}>
+//           <ActivityIndicator />
+//         </View>
+//       );
+//     return (
+//       <View style={styles.container}>
+//         <TextInput
+//           style={styles.textInputStyles}
+//           onChangeText={(text) => this.searchFilterFunction(text)}
+//           value={this.state.text}
+//           underlineColorAndroid="transparent"
+//           placeholder="Search Here"
+//         />
+//         <FlatList
+//           data={this.state.dataSource}
+//           item={this.listViewItemSeparator}
+//           renderItem={({item}) => (
+//             <Text style={styles.textStyles}>{item.title}</Text>
+//           )}
+//           style={{marginTop: 10}}
+//           keyExtractor={(index, item) => index.toString() + item}
+//           ItemSeparatorComponent={() => <View style={styles.separator} />}
+//         />
+//       </View>
+//     );
+//   }
+// }
+
+// export default App;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     marginTop: 40,
+//     padding: 16,
+//   },
+//   textStyles: {
+//     padding: 10,
+//   },
+//   textInputStyles: {
+//     height: 40,
+//     borderWidth: 1,
+//     paddingLeft: 10,
+//     borderColor: '#009688',
+//     backgroundColor: '#FFFFFF',
+//   },
+//   separator: {
+//     height: 0.3,
+//     width: '90%',
+//     backgroundColor: '#080808',
+//   },
+// });
+
+///////////////////////////////////
+
+import * as React from 'react';
+import {Text, View, Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+function homeScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Detail"
+        onPress={() => navigation.navigate('details')}
+      />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    marginTop: 10,
-    marginLeft: 10,
-    marginRight: 10,
-    marginBottom: 30,
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-  },
-});
+function detailScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Detail Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="home">
+        <Stack.Screen name="home" component={homeScreen} />
+        <Stack.Screen name="details" component={detailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
